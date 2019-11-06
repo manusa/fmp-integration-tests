e7169b1f80368a37d768ef3775c349d14fbe6509
Marc Nuri <marc@marcnuri.com>
2019-11-06 07:25:52 +0100

    Multi-profile example application complete

---
+++ generators/spring-boot/multi-profile/src/main/java/com/marcnuri/fmpintegrationtests/generators/springboot/multiprofile/MultiProfileApplication.java
@@ -0,0 +1,20 @@
+/*
+ * MultiProfileApplication.java
+ *
+ * Created on 2019-11-06, 6:56
+ */
+package com.marcnuri.fmpintegrationtests.generators.springboot.multiprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Marc Nuri <marc@marcnuri.com> on 2019-11-06.
+ */
+@SpringBootApplication
+public class MultiProfileApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MultiProfileApplication.class, args);
+  }
+}
